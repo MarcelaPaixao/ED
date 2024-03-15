4cb2e9c7190392ed10d303fe5e97ae68812eef91
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2024-03-14 23:23:47 -0300

    feito

--- revisao/aula1_2.c
+++ revisao/aula1_2.c
@@ -9,7 +9,7 @@ void calc_esfera(float r, float* area, float* volume){
     *volume = (4*PI*r*r*r)/3;
 }
 
-/*int raizes(float a, float b, float c, float* x1, float* x2){
+int raizes(float a, float b, float c, float* x1, float* x2){
     double delta = (b*b) - 4*a*c;
     if(delta < 0){
         return 0;
@@ -17,7 +17,7 @@ void calc_esfera(float r, float* area, float* volume){
     *x1 = ((-1*b) + sqrt(delta))/(2*a);
     *x2 = ((-1*b) - sqrt(delta))/(2*a);
     return 1;
-}*/
+}
 
 int pares (int n, int* vet){
     int qtdPares=0;
@@ -30,10 +30,24 @@ int pares (int n, int* vet){
 }
 
 void inverte (int n, int*vet){
-    
+    int i, j, aux=0;
+    for(i=0, j=n-1; i < n/2; i++, j--){
+        aux = vet[i];
+        vet[i] = vet[j];
+        vet[j] = aux;
+    }
 }
 
-/*
+double avalia (double* poli, int grau, double x){
+    double result=0;
+
+    for(int i=0; i <= grau; i++){
+        result += poli[i] * pow(x, i);
+    }
+
+    return result;
+}
+
 //2.1)
 int main(){
     float r=0, area=0, volume=0;
@@ -43,7 +57,6 @@ int main(){
     printf("Area: %.2f\nVolume:%.2f\n", area, volume);
     return 0;
 }
-*/
 
 /*
 //2.2)
@@ -82,3 +95,60 @@ int main(){
 }
 */
 
+/*
+//2.4)
+int main(){
+    int n=0;
+    printf("Digite o numero de elementos:");
+    scanf("%d", &n);
+
+    int *vet = malloc(n * sizeof(int));
+    
+    printf("Digite os elementos:");
+    for(int i=0; i < n; i++){
+        scanf("%d", &vet[i]);
+    }
+    
+    printf("Vetor passado:");
+    for(int i=0; i < n; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+    printf("Vetor invertido:");
+    inverte(n, vet);
+    for(int i=0; i < n; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+    free(vet);
+    return 0;
+}
+*/
+
+/*
+//2.5)
+int main(){
+    int grau=0;
+    double x=0;
+
+    printf("Digite o grau do polinomio:");
+    scanf("%d", &grau);
+
+    double *poli = malloc((grau + 1) * sizeof(double));
+    
+    printf("Digite os coeficientes:");
+    for(int i=0; i <= grau; i++){
+        scanf("%lf", &poli[i]);
+    }
+    
+    printf("Digite o valor de x:");
+    scanf("%lf", &x);
+
+    printf("O resultado do polinomio e: %.2lf\n", avalia(poli, grau, x));
+
+    free(poli);
+    return 0;
+}
+*/

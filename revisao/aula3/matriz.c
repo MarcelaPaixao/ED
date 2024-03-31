835745e102060f96e985ec7d93338af845886ce4
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2024-03-31 16:42:30 -0300

    aula3 completo

--- revisao/aula3/matriz.c
+++ revisao/aula3/matriz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "matriz.h"
 
 //Tipo matriz (tipo opaco)
@@ -17,24 +16,28 @@ struct matriz {
  */
 Matriz* inicializaMatriz (int nlinhas, int ncolunas){
     if(nlinhas <= 0 || ncolunas <= 0) return NULL;
-
+    
     Matriz *mat = malloc(sizeof(Matriz));
-    if(!mat) printf("Erro na alocacao do tipo Matriz!\n"); return NULL;
+    if(!mat){
+        printf("Erro na alocacao do tipo Matriz!\n"); 
+        exit(1);
+    } 
 
     mat->vet = calloc(nlinhas * ncolunas, sizeof(int));
-    if(!mat->vet) printf("Erro na alocacao da matriz!\n"); return NULL;
+    if(!mat->vet){
+        printf("Erro na alocacao da matriz!\n"); 
+        exit(1);
+    } 
     
     mat->lin = nlinhas;
     mat->col = ncolunas;
-    
-    for(int i=0; i < nlinhas*ncolunas; i++) scanf("%d", &mat->vet[i]);
 
     return mat;
 }
 
 int verificaLimiteMatriz(Matriz *mat, int lin, int col){
-    if(lin <= 0 || lin >= mat->lin ||
-       col <= 0 || col >= mat->col){
+    if(lin < 0 || lin >= mat->lin ||
+       col < 0 || col >= mat->col){
         return 0;
     }
     return 1;
@@ -48,7 +51,7 @@ int verificaLimiteMatriz(Matriz *mat, int lin, int col){
  */
 void modificaElemento (Matriz* mat, int linha, int coluna, int elem){
     if(!mat || !verificaLimiteMatriz(mat, linha, coluna)) return;
-    mat->vet[(mat->col*linha) + coluna] = elem;
+    mat->vet[mat->col*linha + coluna] = elem;
 }
 
 /*Retorna o elemento mat[linha][coluna]
@@ -58,7 +61,7 @@ void modificaElemento (Matriz* mat, int linha, int coluna, int elem){
  * pos-condicao: mat n„o È modificada
  */
 int recuperaElemento(Matriz* mat, int linha, int coluna){
-    if(!mat || !verificaLimiteMatriz(mat, linha, coluna)) return;
+    if(!mat || !verificaLimiteMatriz(mat, linha, coluna)) return 0;
     return (mat->vet[(mat->col*linha) + coluna]);
 }
 
@@ -90,19 +93,15 @@ int recuperaNLinhas (Matriz* mat){
  */
 Matriz* transposta (Matriz* mat){
     if(!mat) return NULL;
-    Matriz *transposta =  malloc(sizeof(Matriz));
-    transposta->lin = mat->col;
-    transposta->col = mat->lin;
+    Matriz *transp =  inicializaMatriz(recuperaNColunas(mat), recuperaNLinhas(mat));
 
-    transposta->vet = calloc(transposta->lin * transposta->col, sizeof(int));
-
-    for(int i=0; i < mat->lin; i++){
-        for(int j=0; j < mat->col; j++){
-            transposta->vet[(transposta->col*j)+i] = mat->vet[(mat->col*i)+j];
+    for(int i=0; i < transp->lin; i++){
+        for(int j=0; j < transp->col; j++){
+            transp->vet[(transp->col*i)+j] = mat->vet[(mat->col*j)+i];
         }
     }
 
-    return transposta;
+    return transp;
 }
 
 /*Retorna a matriz multiplicacao entre mat1 e mat2
@@ -115,22 +114,26 @@ Matriz* transposta (Matriz* mat){
 Matriz* multiplicacao (Matriz* mat1, Matriz* mat2){
     Matriz *mat3 = NULL;
     if(mat1 && mat2 && mat1->col == mat2->lin){
-        mat3 = malloc(sizeof(Matriz));
-        mat3->lin = mat1->lin;
-        mat3->col = mat2->col;
-        
-        mat3->vet = calloc(mat3->lin * mat3->col, sizeof(int));
+        mat3 = inicializaMatriz(mat1->lin, mat2->col);
 
         for(int i=0; i < mat3->lin; i++){
             for(int j=0; j < mat3->col; j++){
                 for(int k=0; k < mat1->col; k++){
-                    mat3->vet[(mat3->col*i)+j] += mat1->vet[(mat1->col*i)+k] + mat2->vet[(mat2->col*k)+j];
+                    mat3->vet[mat3->col*i+j] += mat1->vet[mat1->col*i+k] * mat2->vet[mat2->col*k+j];
                 }
             }
         }
     }
     
     return mat3;
+}
+
+void imprimeLinhas (Matriz* mat, int indice){
+    if(!mat) return;
+    for(int j=0; j < mat->col; j++){
+        printf("%d ", recuperaElemento(mat, indice, j));
+    }
+    printf("\n");
 }
 
 /*Imprime a matriz
@@ -142,17 +145,7 @@ Matriz* multiplicacao (Matriz* mat1, Matriz* mat2){
 void imprimeMatriz(Matriz* mat){
     if(!mat) return;
     for(int i=0; i < mat->lin; i++){
-        for(int j=0; j < mat->col; j++){
-            printf("%d ", mat->vet[(mat->col*i)+j]);
-        }
-        printf("\n");
-    }
-}
-
-void imprimeLinhas (Matriz* mat, int indice){
-    if(!mat) return;
-    for(int j=0; j < mat->col; j++){
-        printf("%d ", mat->vet[(mat->col*indice)+j]);
+        imprimeLinhas(mat, i);
     }
     printf("\n");
 }

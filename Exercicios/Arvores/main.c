8d0cb97ec28d0bdf38b445722c5ca45e5f9942f3
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2024-06-12 16:40:01 -0300

    arvBin

---
+++ Exercicios/Arvores/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvBin.h"
+
+int main(){
+    Aluno *a1 = CriaAluno("Marcela");
+    Aluno *a2 = CriaAluno("Malu");
+    Aluno *a3 = CriaAluno("Aline");
+    Aluno *a4 = CriaAluno("Gabriel");
+    Aluno *a5 = CriaAluno("Eduardo");
+    Aluno *a6 = CriaAluno("Raony");
+
+    Arv *arv1 = NULL;
+    
+
+
+
+
+
+    return 0;
+}
